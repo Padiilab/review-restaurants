315cb149479fd418623eff93b3580c7a687d185f
Padiilab <55683707+Padiilab@users.noreply.github.com>
2021-07-31 23:08:40 +0300

    add hoc and fix some troubles

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,37 +1,70 @@
 module.exports = {
-    extends: [
-        'airbnb-typescript',
-        'airbnb/hooks',
-        'plugin:@typescript-eslint/recommended',
-        'plugin:jest/recommended',
-        'plugin:prettier/recommended'
-    ],
-    plugins: ['react', '@typescript-eslint', 'jest'],
-    env: {
-        browser: true,
-        es6: true,
-        jest: true,
-    },
-    globals: {
-        Atomics: 'readonly',
-        SharedArrayBuffer: 'readonly',
-    },
     parser: '@typescript-eslint/parser',
+    plugins: ['react', '@typescript-eslint', 'prettier'],
+    extends: ['prettier/@typescript-eslint'],
+    overrides: [{
+        files: ["src/**/*.{ts,tsx}"]
+    }],
     parserOptions: {
-        ecmaFeatures: {
-            jsx: true,
-        },
         ecmaVersion: 2018,
         sourceType: 'module',
-        project: './tsconfig.json',
+        ecmaFeatures: {
+            jsx: true
+        }
     },
     rules: {
-        'linebreak-style': 'off',
         'prettier/prettier': [
-            'error',
+            'warn',
             {
-                endOfLine: 'auto',
-            },
+                usePrettierrc: true
+            }
         ],
+        'comma-dangle': ['error', 'never'],
+        'use-isnan': ['error', { enforceForSwitchCase: true }],
+        'react/void-dom-elements-no-children': 'warn',
+        'react/no-unsafe': 'warn',
+        'react/no-unused-state': 'warn',
+        'react/prefer-stateless-function': 'warn',
+        'react/react-in-jsx-scope': 'warn',
+        'react/self-closing-comp': 'warn',
+        'react/no-will-update-set-state': 'warn',
+        'react/no-this-in-sfc': 'warn',
+        'react/no-string-refs': 'warn',
+        'react/no-redundant-should-component-update': 'warn',
+        'react/jsx-boolean-value': ['warn', 'never'],
+        'react/jsx-key': 'warn',
+        'react/jsx-max-props-per-line': ['warn', { maximum: 7 }],
+        'react/jsx-max-depth': ['warn', { max: 8 }],
+        'arrow-body-style': ['warn', 'as-needed'],
+        'dot-notation': 'warn',
+        'jsx-quotes': ['warn', 'prefer-single'],
+        'valid-typeof': 'warn',
+        '@typescript-eslint/member-ordering': [
+            'warn',
+            {
+                default: [
+                    'private-static-field',
+                    'protected-static-field',
+                    'public-static-field',
+                    'private-static-method',
+                    'protected-static-method',
+                    'public-static-method',
+                    'private-constructor',
+                    'protected-constructor',
+                    'public-constructor',
+                    'private-instance-field',
+                    'protected-instance-field',
+                    'public-instance-field',
+                    'private-instance-method',
+                    'protected-instance-method',
+                    'public-instance-method'
+                ]
+            }
+        ]
     },
+    settings: {
+        react: {
+            version: 'detect'
+        }
+    }
 };
